bad2911ad3764f7482cf152a4c9fa83ef4fb335a
sawfly29 <sawfly@yandex.ru>
2020-11-01 15:06:20 +0300

    stage 1

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -2,6 +2,6 @@ import React from 'react';
 //можно передать пропс и вернуть props.todo.title
 export default function TodoItem({todo, index}){
     return (
-        <li><strong>{index + 1}</strong>{todo.title}</li>
+        <li><strong>{index + 1 + ' '}</strong>{todo.title}</li>
     )
 }
